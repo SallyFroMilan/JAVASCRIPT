7ceef7a7dcc28a7ebff3350ca094f8a976d8604a
Sally Caruso <sally_caruso@hotmail.com>
2024-02-14 11:24:47 +0100

    Integrate the missing part on string together different operations getting the rigth answer

--- JS-Fundamentals-Part-5/Project Calculator/Assignment/main.js
+++ JS-Fundamentals-Part-5/Project Calculator/Assignment/main.js
@@ -2,9 +2,12 @@ let operator = '';
 let previousValue = '';
 let currentValue = '';
 
+const previousScreen = document.querySelector('.previous');
+const currentScreen = document.querySelector('.current');
+
 document.addEventListener('DOMContentLoaded', function() {
-    const previousScreen = document.querySelector('.previous');
-    const currentScreen = document.querySelector('.current');
+    
+    window.addEventListener('keydown', handleKeyPress);
 
     let clear = document.querySelector('.clear');
     let equal = document.querySelector('.equal');
@@ -17,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
         handleNumber(e.target.textContent)
         currentScreen.textContent = currentValue;
     }))
+
     operators.forEach((op) => op.addEventListener('click', function(e) {
         handleOperator(e.target.textContent)
         previousScreen.textContent = previousValue + ' ' + operator;
@@ -34,14 +38,9 @@ document.addEventListener('DOMContentLoaded', function() {
     equal.addEventListener('click', function() {
         if (currentValue != '' && previousValue != '') {
             calculate()
-            previousScreen.textContent = '';
-            if (previousValue.length <= 5) {
-                currentScreen.textContent = previousValue;
-            } else {
-                currentScreen.textContent = previousValue.slice(0,5) + '...';
-            }
         }
     })
+
     decimal.addEventListener('click', function() {
         addDecimal();
     }) 
@@ -68,8 +67,23 @@ function handleNumber(num) {
 }
 
 function handleOperator(op) {
-    operator = op;
-    previousValue = currentValue;
+    if (previousValue === '') {
+        previousValue = currentValue;
+        operatorCheck(op);
+      } else if (currentValue === '') {
+        operatorCheck(op);
+      } else {
+        calculate();
+        operator = op;
+        previousScreen.textContent = previousValue + ' ' + operator;
+        currentScreen.textContent = '0';
+    }
+}
+
+function operatorCheck(text) {
+    operator = text;
+    previousScreen.textContent = previousValue + ' ' + operator;
+    currentScreen.textContent = '0';
     currentValue = '';
 }
 
@@ -83,22 +97,76 @@ function calculate() {
         previousValue -= currentValue;
     } else if (operator === 'x') {
         previousValue *= currentValue;
-    } else {
+    } else if (operator === '/') {
+        if (currentValue <= 0) {
+            previousValue = 'Error';
+            displayResults();
+            return;
+        }
         previousValue /= currentValue;
     }
-
     previousValue = roundNumber(previousValue);
     previousValue = previousValue.toString();
-    currentValue = previousValue.toString();
+    displayResults();
 }
 
 function roundNumber(num) {
     return Math.round(num * 1000) / 1000;
 }
 
+function displayResults() {
+    if (previousValue.length <= 11) {
+      currentScreen.textContent = previousValue;
+    } else {
+      currentScreen.textContent = previousValue.slice(0, 11) + '...';
+    }
+    previousScreen.textContent = '';
+    operator = '';
+    currentValue = '';
+}
+
 function addDecimal() {
     if (!currentValue.includes('.')) {
         currentValue += '.';
+    }
+}
+
+function handleKeyPress(e) {
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9) {
+      handleNumber(e.key);
+    }
+    if (
+      e.key === 'Enter' ||
+      (e.key === '=' && currentValue != '' && previousValue != '')
+    ) {
+      calculate();
+    }
+    if (e.key === '+' || e.key === '-' || e.key === '/') {
+      handleOperator(e.key);
+    }
+    if (e.key === '*') {
+      handleOperator('x');
+    }
+    if (e.key === '.') {
+      addDecimal();
+    }
+    if (e.key === 'Backspace') {
+      handleDelete();
+    }
+}
+
+function handleDelete() {
+    if (currentValue !== '') {
+        currentValue = currentValue.slice(0, -1);
+        currentScreen.textContent = currentValue;
+        if (currentValue === '') {
+        currentScreen.textContent = '0';
+        }
+    }
+    if (currentValue === '' && previousValue !== '' && operator === '') {
+      previousValue = previousValue.slice(0, -1);
+      currentScreen.textContent = previousValue;
     }
 }
 
